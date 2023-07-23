b82607d103749792e61d6194fe51565c1fea1118
Aarti Inamdar <inamdaraarti6@gmail.com>
2023-07-23 22:09:56 +0530

    started learning exception

---
+++ exception/src/ExceptionHandlingUsingThrowsKeyword.java
@@ -0,0 +1,22 @@
+/*
+   In Exception hierarchy top level class is Throwable.
+   Exception class extends Throwable
+
+   compile time exception -> IOException extends exception
+   compile time exception -> ClassNotFoundException extends exception
+   compile time exception -> FileNotFoundException extends exception
+
+   run time exception -> RunTimeException extends exception
+
+   ArithmeticException extends RunTimeException
+   NullPointerException extends RunTimeException
+
+ */
+
+
+
+
+
+
+public class ExceptionHandlingUsingThrowsKeyword {
+}
