8f45d8c9e225db90f42af907c7a28296132bd73d
Aarti Inamdar <inamdaraarti6@gmail.com>
2023-07-30 21:10:26 +0530

    Java Generics in 5

---
+++ java-generics/src/ListExampleBeforeJava5.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import java.util.ArrayList;
+
+public class ListExampleBeforeJava5 {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+
+        list.add("Vikram");
+        list.add(100);
+        list.add(10.5);
+
+        for (int i = 0; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+        String secondItem = (String) list.get(1);//we avoid compile time type checking using explicit casting
+        //But at run time this will fail
+        //bz right side value is Integer and left side is String
+        //hence Integer cannot be converted to String
+        //Hence exception ClassCastException
+
+    }
+}
