2d16b5a137ba41b8865cbcc808eca8d811f3d233
Aarti Inamdar <inamdaraarti6@gmail.com>
2023-08-01 13:01:11 +0530

    Java 8 features

--- java-8-features/src/functional/interface1/ExampleInterface.java
+++ java-8-features/src/functional/interface1/ExampleInterface.java
@@ -6,4 +6,17 @@ public interface ExampleInterface {
     default void print(int result){
         System.out.println(result);
     }//default method is a concrete method that is written in functional interface.
+
+    default void print2(){
+        System.out.println("inside print2 method");
+
+    }
+
+    static void showMsg(String msg){
+        System.out.println(msg);
+    }
+
+    static void showMsg2(String msg){
+        System.out.println(msg);
+    }
 }
